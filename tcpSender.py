8e01a119a4a8dbb391a9f15c778bf764ab92dbf3
pohotlivi_ded <wolf.alex.shishkin@gmail.com>
2023-10-29 18:48:44 +0500

    refactor

--- tcpSender.py
+++ tcpSender.py
@@ -5,25 +5,27 @@ from tcpPacket import TcpPacket
 
 
 class TcpSender:
-    def __init__(self, src: str, dst: str, sport: int, dport: int, timeout: int = 2):
+    def __init__(self, src: str, dst: str, sport: int, dport: int, timeout: int = 2, verbose: bool = False):
         self.src = src
         self.dst = dst
         self.sport = sport
         self.dport = dport
         self.timeout = timeout
+        self.verbose = verbose
 
-    def syn(self, data: bytes = b'') -> Packet:
-        return sr1(TcpPacket(self.src, self.dst, self.sport, self.dport, 0x02, data).packet,
-                   timeout=self.timeout, verbose=False)
+    def send(self, flags: int, data: bytes, timeout: int, verbose: bool) -> Packet:
+        return sr1(TcpPacket(self.src, self.dst, self.sport, self.dport, flags, data).packet,
+                   timeout=self.timeout if timeout is None else timeout,
+                   verbose=self.verbose if verbose is None else verbose)
 
-    def ack(self, data: bytes = b'') -> Packet:
-        return sr1(TcpPacket(self.src, self.dst, self.sport, self.dport, 0x10, data).packet,
-                   timeout=self.timeout, verbose=False)
+    def syn(self, data: bytes = b'', timeout: int = None, verbose: bool = None) -> Packet:
+        return self.send(0x02, data, timeout, verbose)
 
-    def finAck(self, data: bytes = b'') -> Packet:
-        return sr1(TcpPacket(self.src, self.dst, self.sport, self.dport, 0x11, data).packet,
-                   timeout=self.timeout, verbose=False)
+    def ack(self, data: bytes = b'', timeout: int = None, verbose: bool = None) -> Packet:
+        return self.send(0x10, data, timeout, verbose)
 
-    def send(self, data: bytes = b'') -> Packet:
-        return sr1(TcpPacket(self.src, self.dst, self.sport, self.dport, 0x18, data).packet,
-                   timeout=self.timeout, verbose=False)
+    def finAck(self, data: bytes = b'', timeout: int = None, verbose: bool = None) -> Packet:
+        return self.send(0x11, data, timeout, verbose)
+
+    def sendData(self, data: bytes = b'', timeout: int = None, verbose: bool = None) -> Packet:
+        return self.send(0x18, data, timeout, verbose)
